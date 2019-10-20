2e1cf1f1accbab2aa7facdd0ca7b35e0e54f7dae
Jhevaunni <jspruil6@uncc.edu>
2019-10-20 06:00:02 +0000

    Initial Commit for Rails Basics 1

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -1,7 +1,20 @@
 # RubyHashes
 # Part I
 def array_2_hash emails, contacts
-    # YOUR CODE HERE
+
+    i=0
+    contacts.each do |k, v|
+
+    if(email[i]==nil) then
+    email[i]=''
+    end
+
+    contacts[k] = email[i]
+    i=i+1
+    end
+    
+    puts contacts
+
 end
 
 # Part II
