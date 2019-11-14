2c35cb236eca3e6b9711f1e145d2bb7587aac759
Jesse Tovar <jtovar@uncc.edu>
2019-11-14 18:29:43 +0000

    Complete Create and Show

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,13 @@
 class UserController < ApplicationController
     def show
-        @user = User.find(params[:user])
+        @user = User.find(params[:id])
     end
     
     def new
     end
     
     def create
-        @user = User.new(params[:user])
+        @user = User.new(user_params)
         
         @user.save
         redirect_to @user
