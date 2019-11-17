b8ff9108dc358ab2db3d5b818b9c327e8e36d470
Jesse Tovar <jtovar@uncc.edu>
2019-11-17 01:05:07 +0000

    Add validations for poll contents

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,2 +1,7 @@
 class Poll < ApplicationRecord
+    validates :Poll_contents, presence: true, length: {minimum: 1}
+    validates :Choice_1, presence: true, length: {minimum: 1}
+    validates :Choice_2, presence: true, length: {minimum: 1}
+    
 end
+
