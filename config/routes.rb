c9c5cab72987ae336bfc4a4b99b8424e0d159574
Jhevaunni <jspruil6@uncc.edu>
2019-11-19 07:11:14 +0000

    add login/logout and new user functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get'home/index'
+  
   resources :users
-  get 'home/index'
-  
-  resources :user
-  
   resources :polls
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  
+   root 'home#index'
 end
