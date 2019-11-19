b65c05f6bf2f980fb92ff31330781476f595c992
QingLian Yu <qyu3@uncc.edu>
2019-11-19 19:11:08 +0000

    change howto to How To

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   get 'howto/index'
   get 'home/index'
-=======
+
   get'home/index'
->>>>>>> c9c5cab72987ae336bfc4a4b99b8424e0d159574
+
   
   resources :users
   resources :polls
@@ -13,13 +13,12 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   
-<<<<<<< HEAD
+
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   
 end
-=======
-   root 'home#index'
-end
->>>>>>> c9c5cab72987ae336bfc4a4b99b8424e0d159574
+
+
+
